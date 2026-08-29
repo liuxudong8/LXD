65d29ef03385dcdced9249036f64409a0d4a75ce
刘旭东 <48508383+liuxudong8@users.noreply.github.com>
2026-08-29 09:15:14 +0800

    Add files via upload

---
+++ RH/保序双射定理/Lean/OrderPreservingBijection.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `OrderPreservingBijection` library.
+-- Import modules here that should be built as part of the library.
+import OrderPreservingBijection.Basic
+import OrderPreservingBijection.HyperbolicIdentity
